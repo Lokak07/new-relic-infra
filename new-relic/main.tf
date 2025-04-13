314005d3a8dcdaefe261c3bc1e3d6a857817a96b
root <root@loka.>
2025-04-13 14:21:20 +0000

    final-changes

--- new-relic/main.tf
+++ new-relic/main.tf
@@ -34,9 +34,9 @@ module "ec2_instance" {
 
   name = var.instance_name
 
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  monitoring    = var.monitoring
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  monitoring                  = var.monitoring
   associate_public_ip_address = true
   vpc_security_group_ids = length(var.security_group) > 0 ? var.security_group : [
     module.security-group_https-443.security_group_id,
