1d3ee0b7d17d0376e3cc1544733ddb89a6b8c32c
Lokak07 <lokashakthi19.ls@gmail.com>
2025-04-15 05:07:55 +0000

    making changes to s3

--- new-relic/backend.tf
+++ new-relic/backend.tf
@@ -1,11 +1,14 @@
+# enabled versioning and state locking on s3
+
+
 terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
-    bucket         = "my-secure-terraform-state-shakthi"
+    bucket         = "my-secure-terraform-state-shakthi-v2"
     key            = "vpc/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    # No dynamodb table — trying to use native locking.
   }
 }
 
