f42b64dc363d64a8d8a33256c6039006450d2369
Lokak07 <lokashakthi19.ls@gmail.com>
2025-04-15 05:14:02 +0000

    fmt failure

--- new-relic/backend.tf
+++ new-relic/backend.tf
@@ -4,10 +4,10 @@
 terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
-    bucket         = "my-secure-terraform-state-shakthi-v2"
-    key            = "vpc/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "my-secure-terraform-state-shakthi-v2"
+    key     = "vpc/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
     # No dynamodb table — trying to use native locking.
   }
 }
